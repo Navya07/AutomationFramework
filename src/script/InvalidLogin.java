8e3a91a998054f74a3d019a7fde2d643001981e9
Navya07 <navyashetty14@yahoo.co.in>
2018-03-13 15:54:42 +0530

    InvalidLogin

--- src/script/InvalidLogin.java
+++ src/script/InvalidLogin.java
@@ -12,24 +12,20 @@ public class InvalidLogin extends BaseTest {
 @Test(priority=2,groups="login")
 public void testInvalidLogin()
 {
-	int rc = Excel.getRowCount(XL_PATH, "InvalidLogin");
-	for(int i=1;i<rc;i++)
+	int r=Excel.getRowCount(XL_PATH, "InvalidLogin");
+	for(int i=1;i<=r;i++)
 	{
-
-	String un = Excel.getValue(XL_PATH, "InvalidLogin", i, 0);
-	String pw = Excel.getValue(XL_PATH, "InvalidLogin", i, 1);
-	String expectedErrmsg = Excel.getValue(XL_PATH, "InvalidLogin", i, 2);
+	String un=Excel.getValue(XL_PATH, "InvalidLogin", i, 0);
+	String pw=Excel.getValue(XL_PATH, "InvalidLogin", i, 1);
+	String expectedMSG=Excel.getValue(XL_PATH, "InvalidLogin", i, 2);
+	
 	LoginPage l=new LoginPage(driver);
-	//Enter Valid UN
-			l.setUserName(un);
-			//Enter Valid pw
-			l.setPassword(pw);
-			//Click login
-			l.clickLogin();
-			//Verify error msg
-			l.verifyErrMsg(expectedErrmsg);
-			
+	
+	l.setUserName(un);
+	l.setPassword(pw);
+	l.clickLogin();
+	
+	l.verifyErrMsg(expectedMSG);
 	}
-
 }
 }
