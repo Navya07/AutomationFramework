9c469f75e37113704b8a1fbe32a319fcdd1bcc12
Navya07 <navyashetty14@yahoo.co.in>
2018-03-13 16:05:36 +0530

    Invalidlogin

--- src/script/InvalidLogin.java
+++ src/script/InvalidLogin.java
@@ -9,23 +9,22 @@ import generic.Excel;
 import page.LoginPage;
 
 public class InvalidLogin extends BaseTest {
-@Test(priority=2,groups="login")
-public void testInvalidLogin()
-{
-	int r=Excel.getRowCount(XL_PATH, "InvalidLogin");
-	for(int i=1;i<=r;i++)
-	{
-	String un=Excel.getValue(XL_PATH, "InvalidLogin", i, 0);
-	String pw=Excel.getValue(XL_PATH, "InvalidLogin", i, 1);
-	String expectedMSG=Excel.getValue(XL_PATH, "InvalidLogin", i, 2);
-	
-	LoginPage l=new LoginPage(driver);
-	
-	l.setUserName(un);
-	l.setPassword(pw);
-	l.clickLogin();
-	
-	l.verifyErrMsg(expectedMSG);
+	@Test(priority=2,groups= {"login"})
+	public void testInvalidLogin(){
+		int rc=Excel.getRowCount(XL_PATH, "InvalidLogin");
+		for(int i=1;i<=rc;i++) {
+		String un=Excel.getValue(XL_PATH,"InvalidLogin",i,0);
+		String pw=Excel.getValue(XL_PATH,"InvalidLogin",i,1);
+		String expectedMSG=Excel.getValue(XL_PATH,"InvalidLogin",i,2);
+		LoginPage l=new LoginPage(driver);
+		//Enter invalid user name
+		l.setUserName(un);
+		//Enter invalid password
+		l.setPassword(pw);
+		//Click login
+		l.clickLogin();
+		//Verify err msg
+		l.verifyErrMsg(expectedMSG);
+		}
 	}
-}
 }
