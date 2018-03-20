24fe437200728de69b68ed5f2e1c08243f06aad0
Navya-Shetty <navyashetty2803@gmail.com>
2018-03-20 11:42:48 +0530

    comment

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -8,6 +8,7 @@ import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest implements IAutoConst{
 	public WebDriver driver;
@@ -16,6 +17,7 @@ public abstract class BaseTest implements IAutoConst{
 		System.setProperty(GECKO_KEY,GECKO_VALUE);
 	}
 	
+	@Parameters({"ip","browser"})
 	@BeforeMethod(alwaysRun=true)
 	public void openApplication() {
 		String appURL=AutoUtil.getProperty(CONFIG_PATH,"URL");
